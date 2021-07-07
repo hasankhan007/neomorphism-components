809a6d140d2d96252e67111622514beccaf4a220
hasankhan007 <hasanmrj6458@gmail.com>
2021-07-08 00:28:09 +0600

    neomorphism

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function($){
+	"use strict";
+	/* preloader */
+	$(window).load(function () {
+		//$("#loading").delay(2000).fadeOut(500);
+		$("#loading").fadeOut(1000);
+	})
+
+
+	 $('#slicknav').slicknav();
+
+
+})(jQuery);
+
+
